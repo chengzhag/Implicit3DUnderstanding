3f6f7282a3f7e406f35e41229da2abb609e179d4
Cheng Zhang <850576570@qq.com>
2021-04-05 11:47:45 +0800

    Fixed bug of get_cuda_version.

--- external/ldif/ldif2mesh/get_cuda_version.py
+++ external/ldif/ldif2mesh/get_cuda_version.py
@@ -10,11 +10,17 @@ import subprocess as sp
 # Cuda compilation tools, release 11.2, V11.2.67
 # Build cuda_11.2.r11.2/compiler.29373293_0
 
+# nvcc output for Cuda 10
+# output = "nvcc: NVIDIA (R) Cuda compiler driver\n" \
+#          "Copyright (c) 2005-2019 NVIDIA Corporation\n" \
+#          "Built on Sun_Jul_28_19:07:16_PDT_2019\n" \
+#          "Cuda compilation tools, release 10.1, V10.1.243\n"
+
 def get_cuda_version():
   try:
     output = sp.check_output(['nvcc', '-V']).decode('utf-8')
     lines = output.split('\n')
-    if not output.find('V11'):
+    if output.find('V11') < 0:
       version_str = lines[-2].split(',')[1].split(' ')[-1]
     else:
       version_str = lines[-3].split(',')[1].split(' ')[-1]
